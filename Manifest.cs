9f1a07c187f684cb902d3432e92a9a320db0b3b0
Peter Keating <peter@moov2.com>
2019-09-23 23:39:40 +0100

    v0.5.2

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@
     Name = "Common Widgets",
     Author = "Etch",
     Website = "https://etchuk.com",
-    Version = "0.5.1"
+    Version = "0.5.2"
 )]
 [assembly: Feature(
     Id = "Etch.OrchardCore.Widgets",
