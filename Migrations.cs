b07c34574c8d1a68d7b6e9faff50e9c6018310e8
Peter Keating <peter@moov2.com>
2020-04-13 01:49:38 +0100

    No longer create content definitions as part of migration
    If the content definitions for the widgets provided are required they
    should either be run manually or be executed as part of another recipe.
    When working on projects we often modify the common widget content
    definitions, which can often require removing certain fields. To
    prevent these being re-added when the module is enabled, the content
    definition recipe no longer runs automatically.
    
    Fixes #25.

--- Migrations.cs
+++ Migrations.cs
@@ -9,15 +9,13 @@ namespace Etch.OrchardCore.Widgets
     public class Migrations : DataMigration
     {
         private readonly IContentDefinitionManager _contentDefinitionManager;
-        private readonly IRecipeMigrator _recipeMigrator;
 
         public Migrations(IContentDefinitionManager contentDefinitionManager, IRecipeMigrator recipeMigrator)
         {
             _contentDefinitionManager = contentDefinitionManager;
-            _recipeMigrator = recipeMigrator;
         }
 
-        public async Task<int> CreateAsync()
+        public int CreateAsync()
         {
             #region Html Attributes
 
@@ -28,38 +26,7 @@ namespace Etch.OrchardCore.Widgets
 
             #endregion HtmlAttributes
 
-            await _recipeMigrator.ExecuteAsync("create.recipe.json", this);
-
-            return 4;
-        }
-
-        public int UpdateFrom1()
-        {
-            // previous recipes were created pre RC1 where content type/part/field
-            // settings had a breaking API change
-            //await _recipeMigrator.ExecuteAsync("1.recipe.json", this);
-
-            return 2;
-        }
-
-        public int UpdateFrom2()
-        {
-            // previous recipes were created pre RC1 where content type/part/field
-            // settings had a breaking API change
-            //await _recipeMigrator.ExecuteAsync("2.recipe.json", this);
-
-            return 3;
-        }
-
-        public async Task<int> UpdateFrom3Async()
-        {
-            #region Run Recipe
-
-            await _recipeMigrator.ExecuteAsync("create.recipe.json", this);
-
-            #endregion Run Recipe
-
-            return 4;
+            return 1;
         }
     }
 }
