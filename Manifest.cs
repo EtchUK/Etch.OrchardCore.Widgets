6f8ae2a45eb8a9893354fcd245ae1a3837860cfc
Peter Keating <pete.keating@etchuk.com>
2021-02-08 20:55:59 +0000

    Add "BackgroundPart"
    Added a new part for storing background properties. Below are the
    different fields that have been added with their intention.
    
    Colour - Using our colour field for choosing a colour from a curated
    palette or going rogue and using a custom colour
    Pattern - Collection of patterns that point to images that should be
    used as a background image
    Invert Text - When using a dark background (or light if the theme is
    dark by default), this flag indicates that the content should use
    inverted colours to avoid bleding with background
    - Fixed - When using a pattern, this can be used to give a parallax
    effct

--- Manifest.cs
+++ Manifest.cs
@@ -11,6 +11,6 @@
     Id = "Etch.OrchardCore.Widgets",
     Name = "Common Widgets",
     Description = "Provides widgets commonly found on web pages.",
-    Dependencies = new[] { "OrchardCore.Widgets", "OrchardCore.Flows", "OrchardCore.Html", "Etch.OrchardCore.Blocks", "Etch.OrchardCore.Fields.CodeField", "Etch.OrchardCore.Fields.ResponsiveMedia" },
+    Dependencies = new[] { "OrchardCore.Widgets", "OrchardCore.Flows", "OrchardCore.Html", "Etch.OrchardCore.Blocks", "Etch.OrchardCore.Fields.CodeField", "Etch.OrchardCore.Fields.ColourField", "Etch.OrchardCore.Fields.ResponsiveMedia" },
     Category = "Content"
 )]
--- Migrations.cs
+++ Migrations.cs
@@ -1,4 +1,8 @@
-﻿using OrchardCore.ContentManagement.Metadata;
+﻿using Etch.OrchardCore.Fields.Colour.Fields;
+using Etch.OrchardCore.Fields.Colour.Settings;
+using OrchardCore.ContentFields.Fields;
+using OrchardCore.ContentFields.Settings;
+using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Data.Migration;
 
@@ -25,6 +29,59 @@ namespace Etch.OrchardCore.Widgets
             #endregion HtmlAttributes
 
             return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            _contentDefinitionManager.AlterPartDefinition("BackgroundPart", part => part
+                .Attachable()
+                .WithDescription("Define background properties.")
+                .WithDisplayName("Background")
+                .WithField("BackgroundColour", field => field
+                    .OfType(nameof(ColourField))
+                    .WithDisplayName("Background Colour")
+                    .WithPosition("0")
+                    .WithSettings(new ColourFieldSettings
+                    {
+                        AllowCustom = true,
+                        AllowTransparent = true,
+                        DefaultValue = "transparent",
+                        UseGlobalColours = true
+                    })
+                )
+                .WithField("BackgroundPattern", field => field
+                    .OfType(nameof(TextField))
+                    .WithDisplayName("Background Pattern")
+                    .WithPosition("1")
+                    .WithEditor("PredefinedList")
+                    .WithSettings(new TextFieldPredefinedListEditorSettings
+                    {
+                        Editor = EditorOption.Dropdown,
+                        Options = new ListValueOption[] { new ListValueOption { Name = "None", Value = string.Empty } }
+                    })
+                )
+                .WithField("InvertTextColour", field => field
+                    .OfType(nameof(BooleanField))
+                    .WithDisplayName("Invert Text Colour")
+                    .WithPosition("2")
+                    .WithSettings(new BooleanFieldSettings
+                    {
+                        Hint = "Change text colour to light, recommended when using a dark background.",
+                        Label = "Invert Text Colour"
+                    })
+                )
+                .WithField("BackgroundFixed", field => field
+                    .OfType(nameof(BooleanField))
+                    .WithDisplayName("Background Fixed")
+                    .WithPosition("3")
+                    .WithSettings(new BooleanFieldSettings
+                    {
+                        Hint = "Fix background in place to give a faux parallax effect.",
+                        Label = "Fix Background Pattern"
+                    })
+                ));
+
+            return 2;
         }
     }
 }
