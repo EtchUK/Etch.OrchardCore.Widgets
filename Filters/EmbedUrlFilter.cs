f10c179bf0a4c6e3b44417cacb3a585b23e480a1
Mehdi Miah <Mehdi.Miah@etchuk.com>
2023-01-19 12:55:14 +0000

    Add logic to convert youtube urls into embed

---
+++ Filters/EmbedUrlFilter.cs
@@ -0,0 +1,34 @@
+﻿using Fluid;
+using Fluid.Values;
+using OrchardCore.Liquid;
+using System.Threading.Tasks;
+
+namespace Etch.OrchardCore.Widgets.Filters
+{
+    public class EmbedUrlFilter : ILiquidFilter
+    {
+        public ValueTask<FluidValue> ProcessAsync(FluidValue input, FilterArguments arguments, LiquidTemplateContext context)
+        {
+            var videoUrl = input.ToObjectValue().ToString();
+
+            if (videoUrl.Contains("youtube.com/watch?v="))
+            {
+                return new ValueTask<FluidValue>(new StringValue(ConvertToEmbed(videoUrl)));
+            }
+
+            return new ValueTask<FluidValue>(new StringValue(videoUrl));
+        }
+
+        public static string ConvertToEmbed(string url)
+        {
+            url = url.Replace("watch?v=", "embed/");
+
+            if (url.Contains('&'))
+            {
+                url = url.Substring(0, url.IndexOf("&"));
+            }
+
+            return url;
+        }
+    }
+}
