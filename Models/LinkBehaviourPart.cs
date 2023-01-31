a1424c3b77dfefcbbcfcda003209e648057d4ba5
Mehdi Miah <Mehdi.Miah@etchuk.com>
2023-01-31 12:20:17 +0000

    Add Part models
    - Remove part creation from recipe
    - update templates + reformatting for liquid

---
+++ Models/LinkBehaviourPart.cs
@@ -0,0 +1,12 @@
+﻿using Etch.OrchardCore.Fields.Code.Fields;
+using OrchardCore.ContentFields.Fields;
+using OrchardCore.ContentManagement;
+
+namespace Etch.OrchardCore.Widgets.Models
+{
+    public class LinkBehaviourPart : ContentPart
+    {
+        public CodeField ClickEvent { get; set; }
+        public TextField OpenIn { get; set; }
+    }
+}
